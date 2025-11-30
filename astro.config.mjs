666e50a50e56da1feaa929e4c747de44ea909401
PabloTorres <ptorresp@ups.edu.ec>
2025-11-30 02:57:51 -0500

    Fix GitHub Actions workflow for Astro deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://github.com/PabloT18/01-fundamentos-astro.io',
-    base: '/01-fundamentos-astro.io',
+    site: 'https://pablot18.github.io',
+    base: '/01-fundamentos-astro',
 });
