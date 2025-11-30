2b52181736e8aea1e9a03b9f8b67250712b35ea6
PabloTorres <ptorresp@ups.edu.ec>
2025-11-30 02:54:08 -0500

    Add initial project setup with GitHub Actions for deployment, Prettier configuration, and multiple Astro components and pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://github.com/PabloT18/01-fundamentos-astro.io',
+    base: '/01-fundamentos-astro.io',
+});
